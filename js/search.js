b2921825013d6bad28800e258eff04e71b39be44
thecafe75000 <hualei3375@gmail.com>
2024-01-06 23:24:21 +0100

    Site updated: 2024-01-06 23:24:19

--- js/search.js
+++ js/search.js
@@ -114,7 +114,7 @@ var searchFunc = function (path, search_id, content_id) {
         });
         str += "</ul>";
         if (str.indexOf('<li>') === -1) {
-          return $resultContent.innerHTML = BTN + "<div class=\"search-result-empty\"><p><i class=\"fe fe-tired\"></i> Aucun contenu trouvé, essayez de changer les termes de recherche~<p></div>";
+          return $resultContent.innerHTML = BTN + "<div class=\"search-result-empty\"><p><i class=\"fe fe-tired\"></i> Aucun contenu trouvé, essayez de changer les termes de recherche ☺<p></div>";
         }
         $resultContent.innerHTML = BTN + str;
       });
